6ec68a1223863cf3c7e042d232251956dbb2e901
dangdng <sandy@idoutv.com>
2018-07-02 18:14:24 +0800

    完善头部文件

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -31,16 +31,69 @@
           <img src="../../../static/images/cm2_btm_icn_account.png" alt="">
         </div>
         <span class='mask'></span>
+        <div class='user-action'>
+          <ul>
+            <li>
+              <i class='icn icn-hm'></i>
+              <a href="">我的主页</a>
+            </li>
+            <li>
+              <i class='icn icn-msg'></i>
+              <a href="">我的消息</a>
+            </li>
+            <li>
+              <i class='icn icn-lv'></i>
+              <a href="">我的等级</a>
+            </li>
+            <li>
+              <i class='icn icn-mbr'></i>
+              <a href="">vip会员</a>
+            </li>
+          </ul>
+          <ul class='ltb'>
+            <li>
+              <i class='icn icn-st'></i>
+              <a href="">个人设置</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <i class='icn icn-ex'></i>
+              <a href="">退出</a>
+            </li>
+          </ul>
+          <i class="arr"></i>
+        </div>
       </div>
     </div>
-    <div class="m-subnav">
+    <div class="m-subnav frist-btn">
+      <ul>
+        <li>
+          <a class='z-slt' href="#">推荐</a>
+        </li>
+        <li>
+          <a href="#">排行榜</a>
+        </li>
+        <li>
+          <a href="#">歌单</a>
+        </li>
+        <li>
+          <a href="#">主播电台</a>
+        </li>
+        <li>
+          <a href="#">歌手</a>
+        </li>
+        <li>
+          <a href="#">新碟上架</a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: "head"
+  name: "topHeader"
 };
 </script>
 
@@ -69,18 +122,22 @@ export default {
   align-content: center;
   height: 70px;
   line-height: 70px;
-  margin: 0;
-  padding: 0;
 }
 
 .m-nav li {
   padding: 0 19px;
   font-size: 0.28rem;
-  list-style: none;
-  text-align: center;
   position: relative;
   display: flex;
   justify-content: center;
+}
+
+.m-nav li:hover {
+  background-color: #000;
+}
+
+.m-nav li:hover a {
+  color: #fff;
 }
 
 .m-nav li a {
@@ -119,23 +176,8 @@ export default {
   height: 13px;
   position: absolute;
   top: 21px;
-  right: -24px;
+  right: -20px;
 }
-
-.m-subnav {
-  height: 5px;
-  background-color: #c20c0c;
-  border-bottom: 1px solid #c20c0c;
-}
-
-/* .triangle {
-  border-top: 6px solid transparent;
-  border-right: 6px solid transparent;
-  border-bottom: 7px solid #c20c0c;
-  border-left: 6px solid transparent;
-  position: absolute;
-  bottom: 0;
-} */
 
 .search {
   background: #fff url("../../assets/images/topbar.png") no-repeat;
@@ -161,20 +203,29 @@ export default {
 
 .contribute {
   background: #242424 url("../../assets/images/topbar.png") no-repeat;
-  background-position: 0 -140px;
+  background-position: 0 -180px;
   width: 90px;
   height: 32px;
   line-height: 32px;
   font-size: 0.24rem;
   color: #ccc;
-  text-align: center;
   padding-left: 22px;
   box-sizing: border-box;
+}
+
+.contribute:hover {
+  background-position: 0 -140px;
 }
 
 .user {
   display: flex;
   align-items: center;
+  position: relative;
+  height: 50px;
+}
+
+.user:hover .user-action {
+  display: block;
 }
 
 .user-head {
@@ -200,5 +251,138 @@ export default {
   width: 12px;
   height: 10px;
   margin-left: 10px;
+}
+
+.user-action {
+  display: none;
+  position: absolute;
+  top: 50px;
+  left: -65px;
+  width: 158px;
+  background: #2b2b2b;
+  border: 1px solid #202020;
+  box-shadow: 0 8px 24px 0 rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+}
+
+.user-action:hover {
+  display: block;
+}
+
+.arr {
+  background: url("../../assets/images/toplist.png") no-repeat;
+  position: absolute;
+  top: -8px;
+  left: 50%;
+  width: 14px;
+  height: 8px;
+  margin-left: -7px;
+  background-position: -20px 0;
+}
+
+.ltb {
+  border: 1px solid #232323;
+  border-width: 1px 0;
+}
+
+.user-action li {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+}
+
+.user-action a {
+  color: #ccc;
+  height: 34px;
+  line-height: 34px;
+}
+
+.user-action .icn {
+  width: 18px;
+  height: 18px;
+  margin-right: 10px;
+}
+
+.user-action li:hover {
+  background: #343434;
+}
+
+.user-action li:hover a {
+  color: #fff;
+}
+
+.user-action li:hover .icn {
+  background-color: #343434;
+}
+
+.icn {
+  background: #2b2b2b url("../../assets/images/toplist.png") no-repeat;
+}
+
+.icn-hm {
+  background-position: 0 -140px;
+}
+
+.icn-msg {
+  background-position: -20px -120px;
+}
+
+.icn-lv {
+  background-position: 0 -100px;
+}
+
+.icn-mbr {
+  background-position: 0 -221px;
+}
+
+.icn-st {
+  background-position: 0 -140px;
+}
+
+.icn-ex {
+  background-position: 0 -200px;
+}
+
+.m-subnav {
+  height: 5px;
+  background-color: #c20c0c;
+  border-bottom: 1px solid #c20c0c;
+}
+
+.frist-btn {
+  height: 34px;
+}
+
+.m-subnav ul {
+  display: flex;
+  height: 34px;
+  justify-content: center;
+  margin-left: -14px;
+}
+
+.m-subnav ul li {
+  display: flex;
+  align-items: center;
+}
+
+.m-subnav ul li:hover a {
+  background: #9b0909;
+}
+
+.m-subnav ul a {
+  color: #fff;
+  text-decoration: none;
+  height: 20px;
+  padding: 0 13px;
+  margin: 0 17px;
+  border-radius: 20px;
+  line-height: 21px;
+  display: inline-block;
+}
+
+.z-slt {
+  background: #9b0909;
 }
 </style>
